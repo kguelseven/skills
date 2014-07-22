cdf91920556820d25f83a79d451213d4739c18be
kguelseven <kguelseven@gmail.com>
2014-07-22 20:08:56 +0200

    fixed bower and grunt config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,26 +9,27 @@
  * Main module of the application.
  */
 angular
-    .module('skillsJsApp', [
-        'ngAnimate',
-        'ngCookies',
-        'ngResource',
-        'ngRoute',
-        'ngSanitize',
-        'ngTouch',
-        'ui.bootstrap'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainController'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+	.module('skillsJsApp', [
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'ui.bootstrap',
+		, 'ngGrid'
+	])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainController'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,22 +8,25 @@
  * Controller of the skillsJsApp
  */
 angular.module('skillsJsApp')
-  .controller('MainController', function ($scope, skillService, mitarbeiterService) {
+	.controller('MainController', function ($scope, skillService, mitarbeiterService) {
 
-		$scope.teamChanged = function() {
+		$scope.teamChanged = function () {
 			$scope.mitarbeiters = mitarbeiterService.loadMitarbeiterByTeam($scope.teamSelected.id);
 			$scope.mitarbeiterSelected = $scope.mitarbeiters[0];
 			$scope.mitarbeiterChanged();
 		};
 
-		$scope.mitarbeiterChanged = function() {
+		$scope.mitarbeiterChanged = function () {
 			$scope.skills = skillService.loadSkillsByMitarbeiter($scope.mitarbeiterSelected.id);
 		};
 
-		$scope.predicate = '-name';
+		// $scope.predicate = '-name';
 
 
 		$scope.teams = mitarbeiterService.loadTeams();
 		$scope.teamSelected = $scope.teams[0];
 		$scope.teamChanged();
+
+
+		$scope.gridOptions = {data: 'skills', showFooter: true, maxHeight: 10000};
 	});
