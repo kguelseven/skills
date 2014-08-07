9f0974ef244f875363d2339b9e1d18f32de9ccd5
kguelseven <kguelseven@gmail.com>
2014-08-07 12:26:23 +0200

    wrong url

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@ angular
         'ui.bootstrap',
         'ngGrid'
     ])
-    .constant('REST_URL', 'http://localhost:8080')
+    .constant('REST_URL', '')
     .config(['$provide', '$routeProvider', '$locationProvider', function ($provide, $routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
