8b25e573b1e5911546561c7045ba0cc6af39e515
kguelseven <kguelseven@gmail.com>
2014-08-08 14:37:38 +0200

    proxy-text

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,41 +9,41 @@
  * Main module of the application.
  */
 angular
-    .module('skillsJsApp', [
-        'ngResource',
-        'ngRoute',
-        'ui.bootstrap',
-        'ngGrid'
-    ])
-    .constant('REST_URL', '')
-    .config(['$provide', '$routeProvider', '$locationProvider', function ($provide, $routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/main.html',
-                controller: 'MainController'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutController'
-            })
-            .when('/teams', {
-                templateUrl: 'views/teams.html',
-                controller: 'TeamsController'
-            })
-            .when('/skills', {
-                templateUrl: 'views/skills.html',
-                controller: 'SkillsController'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-        /*
-         $provide.decorator("$exceptionHandler", function ($delegate) {
-         return function (exception, cause) {
-         $delegate(exception, cause);
-         // TODO exception handling
-         };
-         });
-         */
-        // $locationProvider.html5Mode(true);
-    }]);
+	.module('skillsJsApp', [
+		'ngResource',
+		'ngRoute',
+		'ui.bootstrap',
+		'ngGrid'
+	])
+	.constant('REST_URL', '')
+	.config(['$provide', '$routeProvider', '$locationProvider', function ($provide, $routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainController'
+			})
+			.when('/about', {
+				templateUrl: 'views/about.html',
+				controller: 'AboutController'
+			})
+			.when('/teams', {
+				templateUrl: 'views/teams.html',
+				controller: 'TeamsController'
+			})
+			.when('/skills', {
+				templateUrl: 'views/skills.html',
+				controller: 'SkillsController'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		/*
+		 $provide.decorator("$exceptionHandler", function ($delegate) {
+		 return function (exception, cause) {
+		 $delegate(exception, cause);
+		 // TODO exception handling
+		 };
+		 });
+		 */
+		// $locationProvider.html5Mode(true);
+	}]);
